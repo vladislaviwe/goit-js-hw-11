471d8c8ecec6eb241b2775064e7ae8ed2cb1fe91
Vladislav Klymenko <vladislaviwe@outlook.com>
2022-08-19 15:23:42 +0300

    1

--- src/get-refs.js
+++
@@ -1,7 +0,0 @@
-export default function getRefs() {
-    return {
-        input: document.querySelector('#search-box'),
-        countryList: document.querySelector('.country-list'),
-        countryCard: document.querySelector('.country-info'),
-    };
-}
