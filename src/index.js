bf79accd88688f90e4acef68664d68f9cc0c331a
Vladislav Klymenko <vladislaviwe@outlook.com>
2022-08-22 12:55:29 +0300

    Completed homework

--- src/index.js
+++ src/index.js
@@ -2,38 +2,47 @@ import './css/styles.css';
 import API from './fetch-images';
 import getRefs from './getRefs';
 import MARKUP from './markup';
-import axios from 'axios';
 import Notiflix from 'notiflix';
 import simpleLightbox from 'simplelightbox';
 import "simplelightbox/dist/simple-lightbox.min.css";
 
-// const BASE_URL = 'https://pixabay.com/api/';
-// const API_KEY = '29393857-621a457e4e6b44715a66c2c62';
-// const OPTIONS = '&image_type=photo&orientation=horizontal&safesearch=true&';
-// const PER_PAGE = 39;
-// let page = 1;
-
-// export async function fetchImages(nameToSearch) {
-//     try {
-//         const response = await axios.get(`${BASE_URL}?key=${API_KEY}${OPTIONS}q=dog&page=${page}&per_page=${PER_PAGE}`);
-//         console.log(response.data.hits);
-//         return response.data.hits;
-//     } catch (error) {
-//         console.log(error);
-//     }
-// }
-
 const refs = getRefs();
+let lightbox;
+let page = 1;
+let nameToSearch = "";
 
-refs.button.addEventListener('click', () => doSearch());
+refs.loadMoreButton.classList.add('invisible');
 
-function doSearch() {
-    const nameToSearch = refs.input.value.trim().toLowerCase();
+refs.form.addEventListener('submit', doSearch);
+refs.loadMoreButton.addEventListener('click', onLoadMore);
 
-    API.fetchImages(nameToSearch)
-    .then(images => {
-        createMarkup(images);
-    })
+function doSearch(e) {
+    e.preventDefault();
+
+    pageClearing();
+
+    if (page > 0) {
+        page = 1;
+    }
+
+    refs.loadMoreButton.classList.add('invisible');
+
+    nameToSearch = refs.input.value.trim().toLowerCase();
+
+    onFetch();
+    
+    e.target.reset();
+}
+
+function onLoadMore() {
+
+    refs.loadMoreButton.classList.add('invisible');
+
+    lightbox.refresh();
+
+    page += 1;
+
+    onFetch();
 }
 
 const insertMarkup = (element) => {
@@ -45,3 +54,26 @@ function createMarkup(images) {
             insertMarkup(element);
     });     
 }
+
+function pageClearing() {
+    if (refs.imagesList.innerHTML !== "") {
+        refs.imagesList.innerHTML = "";
+    }
+} 
+
+function onFetch() {
+    API.fetchImages(nameToSearch, page)
+    .then(images => {
+        createMarkup(images);
+        if (images.length === 39) {
+            console.log(images.length);
+            refs.loadMoreButton.classList.remove('invisible');
+        }
+        lightbox = new SimpleLightbox('.gallery_link', {
+            captionsData: 'alt',
+            captionPosition: 'bottom',
+            captionDelay: 250,
+        });
+    })
+}
+
--- src/markup.js
+++ src/markup.js
@@ -1,20 +1,26 @@
 export default cardMarkup = (element) => 
 `
 <div class="photo-card">
-<img src="${element.userImageURL}" alt="${element.tags}" loading="lazy" />
-<div class="info">
-  <p class="info-item">
-    <b>Likes: ${element.likes}</b>
-  </p>
-  <p class="info-item">
-    <b>Views: ${element.views}</b>
-  </p>
-  <p class="info-item">
-    <b>Comments: ${element.comments}</b>
-  </p>
-  <p class="info-item">
-    <b>Downloads: ${element.downloads}</b>
-  </p>
-</div>
-</div>
+  <a href="${element.largeImageURL}" class="gallery_link">
+    <img class="photo-card_img" src="${element.webformatURL}" alt="${element.tags}" height="270px" loading="lazy" />
+  </a>
+  <div class="info">
+    <p class="info-item">
+      <b>Likes: </b>
+      <br>${element.likes}</br>
+    </p>
+    <p class="info-item">
+      <b>Views: </b>
+      <br>${element.views}</br>
+    </p>
+    <p class="info-item">
+      <b>Comments: </b>
+      <br>${element.comments}</br>
+    </p>
+    <p class="info-item">
+      <b>Downloads: </b>
+      <br>${element.downloads}</br>
+    </p>
+  </div>
+</div>  
 `
